5e6ae8e9324a2e08c923595ba4250232050e7b2f
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-06-06 12:50:24 +0300

    updated code style fore

--- tests/convertToRPN.test.js
+++ tests/convertToRPN.test.js
@@ -1,8 +1,9 @@
 const convertToRPN = require('../src/convertToRPN');
 
-const expression = "2 * ( 3 + 6 / 2 ) / 4";
-const expectedResult = "2 3 6 2 / + * 4 /";
+const expression = '2 * ( 3 + 6 / 2 ) / 4';
+const expectedResult = '2 3 6 2 / + * 4 /';
 
 test('convert expression "2 * ( 3 + 6 / 2 ) / 4" to RPN "2 3 6 2 / + * 4 /"', () => {
-    expect(convertToRPN(expression)).toBe(expectedResult);
-  });
+    expect(convertToRPN(expression))
+        .toBe(expectedResult);
+});

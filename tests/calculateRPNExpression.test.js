1549bcd0c8b24f206b61628c7b756ac0f8f2755f
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-06-06 14:23:09 +0300

    update test descriptio in calculate expression test

--- tests/calculateRPNExpression.test.js
+++ tests/calculateRPNExpression.test.js
@@ -2,7 +2,7 @@ const calculateRPNExpression = require('../src/calculateRPNExpression');
 
 const expression = '2 3 6 2 / + * 4 /';
 
-test('convert expression "2 * ( 3 + 6 / 2 ) / 4" to RPN "2 3 6 2 / + * 4 /"', () => {
+test('calculate RPN expression "2 3 6 2 / + * 4 /" expected 3', () => {
     expect(calculateRPNExpression(expression))
         .toBe(3);
 });

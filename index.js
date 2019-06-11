32d12949e4e6eddde21f0e93b8cfdd572745a4f4
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-06-11 12:12:28 +0300

    console query is updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Input math expression for example "1 + 2 - 3 * ( 4 + 1 )" ', (expression) => {
+rl.question('Input math expression for example "1 + 2 - 3 * ( 4 + 1 )". ', (expression) => {
     const rpnExpression = rpnCalculator.convertToRPN(expression);
     const calcResult = rpnCalculator.calculateRPNExpression(rpnExpression);
     console.log(`${expression} = ${calcResult}`);

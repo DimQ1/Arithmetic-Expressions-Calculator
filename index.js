6f67c7113ced0b738a0f8e6f27087543059a1fb9
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-06-11 11:52:26 +0300

    added console input and option start

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-console */
+const readline = require('readline');
 const Calculator = require('./src/Calculator');
-
-const expression = '1 * 2 + 3 * 2 + ( 3 + 6 / 2 ) / 4 - 1 + 2 * ( 2 * 3 - 8 )';
-const calcRes=1 * 2 + 3 * 2 + (3 + 6 / 2) / 4 - 1 + 2 * (2 * 3 - 8);
-
 
 const rpnCalculator = new Calculator();
 
-const rpnExpression = rpnCalculator.convertToRPN(expression);
-const calcResult = rpnCalculator.calculateRPNExpression(rpnExpression);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log(rpnExpression);
-console.log(calcResult);
-console.log(calcRes);
+rl.question('Input math expression for example "1 + 2 - 3 * ( 4 + 1 )" ', (expression) => {
+    const rpnExpression = rpnCalculator.convertToRPN(expression);
+    const calcResult = rpnCalculator.calculateRPNExpression(rpnExpression);
+    console.log(`${expression} = ${calcResult}`);
+    rl.close();
+});
